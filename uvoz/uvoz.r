e3046ae3ec7f2ba9afc10e54d9f11523b7e654d3
Eva Rozman <eva.rozman1312@gmail.com>
2019-01-13 21:46:17 +0100

    Izločitev šumnikov, popravek pri imenih držav; uvoz zemljevida.

--- uvoz/uvoz.r
+++ uvoz/uvoz.r
@@ -11,49 +11,55 @@ zivljenska_doba <- zivljenska_doba[, -c(3, 5)]
 
 moski_zdrava_leta <- filter(zivljenska_doba, INDIC_HE == "Healthy life years in absolute value at birth - males")
 moski_zdrava_leta["SPOL"] <- "m"
-moski_zdrava_leta <- moski_zdrava_leta[, -c(1)]
-names(moski_zdrava_leta) <- (c("DRŽAVA", "ZDRAVA LETA", "SPOL"))
+moski_zdrava_leta <- moski_zdrava_leta[-c(2), -c(1)]
+names(moski_zdrava_leta) <- (c("DRZAVA", "ZDRAVA LETA", "SPOL"))
+moski_zdrava_leta$DRZAVA <- gsub("^Germany.*", "Germany", moski_zdrava_leta$DRZAVA)
 
 zenske_zdrava_leta <- filter(zivljenska_doba, INDIC_HE == "Healthy life years in absolute value at birth - females")
 zenske_zdrava_leta["SPOL"] <- "f"
-zenske_zdrava_leta <- zenske_zdrava_leta[, -c(1)]
-names(zenske_zdrava_leta) <- (c("DRŽAVA", "ZDRAVA LETA", "SPOL"))
+zenske_zdrava_leta <- zenske_zdrava_leta[-c(2), -c(1)]
+names(zenske_zdrava_leta) <- (c("DRZAVA", "ZDRAVA LETA", "SPOL"))
+zenske_zdrava_leta$DRZAVA <- gsub("^Germany.*", "Germany", zenske_zdrava_leta$DRZAVA)
 
 moski_zivlj_doba <- filter(zivljenska_doba, INDIC_HE == "Life expectancy in absolute value at birth - males")
 moski_zivlj_doba["SPOL"] <- "m"
-moski_zivlj_doba <- moski_zivlj_doba[, -c(1)]
-names(moski_zivlj_doba) <- (c("DRŽAVA", "ŽIVLJENSKA DOBA", "SPOL"))
+moski_zivlj_doba <- moski_zivlj_doba[-c(2), -c(1)]
+names(moski_zivlj_doba) <- (c("DRZAVA", "ZIVLJENSKA DOBA", "SPOL"))
+moski_zivlj_doba$DRZAVA <- gsub("^Germany.*", "Germany", moski_zivlj_doba$DRZAVA)
 
 zenske_zivlj_doba <- filter(zivljenska_doba, INDIC_HE == "Life expectancy in absolute value at birth - females")
 zenske_zivlj_doba["SPOL"] <- "f"
-zenske_zivlj_doba <- zenske_zivlj_doba[, -c(1)]
-names(zenske_zivlj_doba) <- (c("DRŽAVA", "ŽIVLJENSKA DOBA", "SPOL"))
+zenske_zivlj_doba <- zenske_zivlj_doba[-c(2), -c(1)]
+names(zenske_zivlj_doba) <- (c("DRZAVA", "ZIVLJENSKA DOBA", "SPOL"))
+zenske_zivlj_doba$DRZAVA <- gsub("^Germany.*", "Germany", zenske_zivlj_doba$DRZAVA)
 
 zdrava_leta <- rbind(moski_zdrava_leta, zenske_zdrava_leta)
 zivlj_doba <- rbind(moski_zivlj_doba, zenske_zivlj_doba)
 
-zdravstveno_stanje <- inner_join(zdrava_leta, zivlj_doba, by=c("DRŽAVA", "SPOL"))
+zdravstveno_stanje <- inner_join(zdrava_leta, zivlj_doba, by=c("DRZAVA", "SPOL"))
 
 st_prebivalcev <- read_csv("podatki/st_prebiv.csv", na=c(":", " : "), locale=locale(encoding="Windows-1250"))
-st_prebivalcev <- st_prebivalcev[, -c(1, 3, 5)]
-names(st_prebivalcev) <- (c("DRŽAVA", "ŠTEVILO PREBIVALCEV"))
+st_prebivalcev <- st_prebivalcev[-c(2), -c(1, 3, 5)]
+names(st_prebivalcev) <- (c("DRZAVA", "STEVILO PREBIVALCEV"))
+st_prebivalcev$DRZAVA <- gsub("^Germany.*", "Germany", st_prebivalcev$DRZAVA)
 
 umrljivost_otrok <- read_csv("podatki/umrljivost_otrok.csv", na=c(":", " : "), locale=locale(encoding="Windows-1250"))
 umrljivost_otrok <- umrljivost_otrok[, -c(3, 4, 5, 6, 7, 9)]
 umrljivost_otrok <- umrljivost_otrok[-c(2, 35),]
+names(umrljivost_otrok) <- (c("SPOL", "DRZAVA", "STEVILO SMRTI OTROK"))
+umrljivost_otrok$DRZAVA <- gsub("^Germany.*", "Germany", umrljivost_otrok$DRZAVA)
 
-names(umrljivost_otrok) <- (c("SPOL", "DRŽAVA", "ŠTEVILO SMRTI OTROK"))
 
-umrljivost_otrok_na_100000 <- inner_join(st_prebivalcev, umrljivost_otrok, by="DRŽAVA")
-umrljivost_otrok_na_100000["ŠTEVILO SMRTI OTROK NA 100.000 PREBIVALCEV"] <- umrljivost_otrok_na_100000["ŠTEVILO SMRTI OTROK"] * 
-  100000 / umrljivost_otrok_na_100000["ŠTEVILO PREBIVALCEV"]
+umrljivost_otrok_na_100000 <- inner_join(st_prebivalcev, umrljivost_otrok, by="DRZAVA")
+umrljivost_otrok_na_100000["STEVILO SMRTI OTROK NA 100.000 PREBIVALCEV"] <- umrljivost_otrok_na_100000["STEVILO SMRTI OTROK"] * 
+  100000 / umrljivost_otrok_na_100000["STEVILO PREBIVALCEV"]
 
 umrljivost_otrok_na_100000$SPOL[umrljivost_otrok_na_100000$SPOL == "Males"] <- "m"
 umrljivost_otrok_na_100000$SPOL[umrljivost_otrok_na_100000$SPOL == "Females"] <- "f"
 
 umrljivost_otrok_na_100000 <- umrljivost_otrok_na_100000[, -c(2, 4)]
 
-zdravstveno_stanje <- inner_join(zdravstveno_stanje, umrljivost_otrok_na_100000, by=c("DRŽAVA", "SPOL"))
+zdravstveno_stanje <- inner_join(zdravstveno_stanje, umrljivost_otrok_na_100000, by=c("DRZAVA", "SPOL"))
 
 
 
@@ -61,66 +67,76 @@ zdravstveno_stanje <- inner_join(zdravstveno_stanje, umrljivost_otrok_na_100000,
 
 proracun <- read_csv("podatki/izdatki.csv", na=c(":", " : "), locale=locale(encoding="Windows-1250"))
 proracun <- proracun[, -c(3, 4, 6)]
-names(proracun) <- (c("LETO", "DRŽAVA", "PRORAČUN NA PREBIVALCA"))
+names(proracun) <- (c("LETO", "DRZAVA", "PRORACUN NA PREBIVALCA"))
+proracun$DRZAVA <- gsub("^Germany.*", "Germany", proracun$DRZAVA)
 
 st_postelj <- read_csv("podatki/st_postelj.csv", na=c(":", " : "), locale=locale(encoding="Windows-1250"))
 st_postelj <- st_postelj[, -c(3, 4, 6)]
-names(st_postelj) <- (c("LETO", "DRŽAVA", "ŠTEVILO POSTELJ"))
+names(st_postelj) <- (c("LETO", "DRZAVA", "STEVILO POSTELJ"))
+st_postelj$DRZAVA <- gsub("^Germany.*", "Germany", st_postelj$DRZAVA)
 
-zdravstvo <- inner_join(proracun, st_postelj, by=c("LETO", "DRŽAVA"))
+zdravstvo <- inner_join(proracun, st_postelj, by=c("LETO", "DRZAVA"))
 
 st_operacijskih_sob <- read_csv("podatki/st_operacijskih_sob.csv", na=c(":", " : "), locale=locale(encoding="Windows-1250"))
 st_operacijskih_sob <- st_operacijskih_sob[, -c(3, 4, 6)]
-names(st_operacijskih_sob) <- (c("LETO", "DRŽAVA", "ŠTEVILO OPERACIJSKIH SOB"))
+names(st_operacijskih_sob) <- (c("LETO", "DRZAVA", "STEVILO OPERACIJSKIH SOB"))
+st_operacijskih_sob$DRZAVA <- gsub("^Germany.*", "Germany", st_operacijskih_sob$DRZAVA)
 
-zdravstvo <- inner_join(zdravstvo, st_operacijskih_sob, by=c("LETO", "DRŽAVA"))
+zdravstvo <- inner_join(zdravstvo, st_operacijskih_sob, by=c("LETO", "DRZAVA"))
 
 st_osebja <- read_csv("podatki/st_zdravnikov_sester.csv", na=c(":", " : "), locale=locale(encoding="Windows-1250"))
 st_osebja <- st_osebja[, -c(4, 6)]
 
 st_zdravnikov <- filter(st_osebja, ISCO08 == "Medical doctors")
 st_zdravnikov <- st_zdravnikov[, -c(1)]
-names(st_zdravnikov) <- (c("LETO", "DRŽAVA", "ŠTEVILO ZDRAVNIKOV"))
+names(st_zdravnikov) <- (c("LETO", "DRZAVA", "STEVILO ZDRAVNIKOV"))
+st_zdravnikov$DRZAVA <- gsub("^Germany.*", "Germany", st_zdravnikov$DRZAVA)
 
-zdravstvo <- inner_join(zdravstvo, st_zdravnikov, by=c("LETO", "DRŽAVA"))
+zdravstvo <- inner_join(zdravstvo, st_zdravnikov, by=c("LETO", "DRZAVA"))
 
 st_zdr_tehnikov <- filter(st_osebja, ISCO08 == "Nursing professionals and midwives")
 st_zdr_tehnikov <- st_zdr_tehnikov[, -c(1)]
-names(st_zdr_tehnikov) <- (c("LETO", "DRŽAVA", "ŠTEVILO ZDRAVSTVENIH TEHNIKOV"))
+names(st_zdr_tehnikov) <- (c("LETO", "DRZAVA", "STEVILO ZDRAVSTVENIH TEHNIKOV"))
+st_zdr_tehnikov$DRZAVA <- gsub("^Germany.*", "Germany", st_zdr_tehnikov$DRZAVA)
 
-zdravstvo <- inner_join(zdravstvo, st_zdr_tehnikov, by=c("LETO", "DRŽAVA"))
+zdravstvo <- inner_join(zdravstvo, st_zdr_tehnikov, by=c("LETO", "DRZAVA"))
 
 st_studentov <- read_csv("podatki/st_studentov.csv", na=c(":", " : "), locale=locale(encoding="Windows-1250"))
 st_studentov <- st_studentov[, -c(4, 6)]
 
 st_stud_medicine <- filter(st_studentov, ISCO08 == "Medical doctors")
 st_stud_medicine <- st_stud_medicine[, -c(1)]
-names(st_stud_medicine) <- (c("LETO", "DRŽAVA", "ŠTEVILO ŠTUDENTOV MEDICINE"))
+names(st_stud_medicine) <- (c("LETO", "DRZAVA", "STEVILO STUDENTOV MEDICINE"))
+st_stud_medicine$DRZAVA <- gsub("^Germany.*", "Germany", st_stud_medicine$DRZAVA)
 
-zdravstvo <- inner_join(zdravstvo, st_stud_medicine, by=c("LETO", "DRŽAVA"))
+zdravstvo <- inner_join(zdravstvo, st_stud_medicine, by=c("LETO", "DRZAVA"))
 
 st_stud_zdravstva <- filter(st_studentov, ISCO08 == "Nurses")
 st_stud_zdravstva <- st_stud_zdravstva[, -c(1)]
-names(st_stud_zdravstva) <- (c("LETO", "DRŽAVA", "ŠTEVILO ŠTUDENTOV ZDRAVSTVA"))
+names(st_stud_zdravstva) <- (c("LETO", "DRZAVA", "STEVILO STUDENTOV ZDRAVSTVA"))
+st_stud_zdravstva$DRZAVA <- gsub("^Germany.*", "Germany", st_stud_zdravstva$DRZAVA)
 
-zdravstvo <- inner_join(zdravstvo, st_stud_zdravstva, by=c("LETO", "DRŽAVA"))
+zdravstvo <- inner_join(zdravstvo, st_stud_zdravstva, by=c("LETO", "DRZAVA"))
 
 
 ### 3. TABELA
 
 BDP <- read_csv("podatki/BDP.csv", na=c(":", " : "), locale=locale(encoding="Windows-1250"))
 BDP <- BDP[, -c(3, 4, 6)]
-names(BDP) <- (c("LETO", "DRŽAVA", "BDP"))
+names(BDP) <- (c("LETO", "DRZAVA", "BDP"))
+BDP$DRZAVA <- gsub("^Germany.*", "Germany", BDP$DRZAVA)
 
 dohodek <- read_csv("podatki/placa.csv", na=c(":", " : "), locale=locale(encoding="Windows-1250"))
 dohodek <- dohodek[, -c(1, 3, 4, 5, 7, 8, 9, 10, 11, 12, 14, 15)]
-names(dohodek) <- (c("DRŽAVA", "LETO", "DOHODEK"))
+names(dohodek) <- (c("DRZAVA", "LETO", "DOHODEK"))
+dohodek$DRZAVA <- gsub("^Germany.*", "Germany", dohodek$DRZAVA)
 
-razvitost <- inner_join(BDP, dohodek, by=c("LETO", "DRŽAVA"))
+razvitost <- inner_join(BDP, dohodek, by=c("LETO", "DRZAVA"))
 
 brezposelnost <- read_csv("podatki/brezposelnost.csv", na=c(":", " : "), locale=locale(encoding="Windows-1250"))
 brezposelnost <- brezposelnost[, -c(3, 4, 5, 7)]
-names(brezposelnost) <- (c("LETO", "DRŽAVA", "ŠTEVILO BREZPOSELNIH"))
+names(brezposelnost) <- (c("LETO", "DRZAVA", "STEVILO BREZPOSELNIH"))
+brezposelnost$DRZAVA <- gsub("^Germany.*", "Germany", brezposelnost$DRZAVA)
 
-razvitost <- inner_join(razvitost, brezposelnost, by=c("LETO", "DRŽAVA"))
+razvitost <- inner_join(razvitost, brezposelnost, by=c("LETO", "DRZAVA"))
 
--- vizualizacija/vizualizacija.r
+++ vizualizacija/vizualizacija.r
@@ -1,13 +1,26 @@
 # 3. faza: Vizualizacija podatkov
 
-# Uvozimo zemljevid.
-zemljevid <- uvozi.zemljevid("http://baza.fmf.uni-lj.si/OB.zip", "OB",
-                             pot.zemljevida="OB", encoding="Windows-1250")
-levels(zemljevid$OB_UIME) <- levels(zemljevid$OB_UIME) %>%
-  { gsub("Slovenskih", "Slov.", .) } %>% { gsub("-", " - ", .) }
-zemljevid$OB_UIME <- factor(zemljevid$OB_UIME, levels=levels(obcine$obcina))
-zemljevid <- fortify(zemljevid)
+# grafi
 
-# Izračunamo povprečno velikost družine
-povprecja <- druzine %>% group_by(obcina) %>%
-  summarise(povprecje=sum(velikost.druzine * stevilo.druzin) / sum(stevilo.druzin))
+### graf 
+
+
+
+# zemljevid
+
+source("https://raw.githubusercontent.com/jaanos/APPR-2018-19/master/lib/uvozi.zemljevid.r")
+
+library(rgdal)
+library(rgeos)
+library(mosaic)
+library(maptools)
+
+zemljevid <- uvozi.zemljevid("http://www.naturalearthdata.com/http//www.naturalearthdata.com/download/50m/cultural/ne_50m_admin_0_countries.zip",
+                             "ne_50m_admin_0_countries", mapa = "zemljevidi", pot.zemljevida = "", encoding = "UTF-8") %>% 
+  fortify()
+
+zemljevid <- filter(zemljevid, CONTINENT == "Europe", long < 55 & long > -45 & lat > 30 & lat < 75)
+
+ggplot() + geom_polygon(data=zemljevid, aes(x=long, y=lat, group=group, fill=id)) +
+  guides(fill=FALSE) + labs(title="Evropa - osnovna slika")
+
